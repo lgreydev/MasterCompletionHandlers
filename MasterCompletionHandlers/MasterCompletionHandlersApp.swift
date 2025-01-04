a39e67e3de0ecffb68191b190cb6593e979289e1
Serhii Lukashchuk <s.lukaschuk@yahoo.com>
2025-01-04 18:02:43 -0500

    Initial Commit

---
+++ MasterCompletionHandlers/MasterCompletionHandlersApp.swift
@@ -0,0 +1,17 @@
+//
+//  MasterCompletionHandlersApp.swift
+//  MasterCompletionHandlers
+//
+//  Created by Serhii Lukashchuk on 2025-01-04.
+//
+
+import SwiftUI
+
+@main
+struct MasterCompletionHandlersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
