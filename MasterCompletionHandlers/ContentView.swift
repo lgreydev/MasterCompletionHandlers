318a65c8dd17872d11db95084c2be9e3a823d38e
Serhii Lukashchuk <s.lukaschuk@yahoo.com>
2025-01-04 18:09:09 -0500

    init

--- MasterCompletionHandlers/ContentView.swift
+++ MasterCompletionHandlers/ContentView.swift
@@ -8,14 +8,27 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var name: String = "world"
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Hello, \(name)!")
+            Button("Tap me") {
+                buttonTapped()
+            }
         }
         .padding()
+    }
+    
+    func buttonTapped() {
+        
+    }
+    
+    func delay(_ seconds: Double, closure: @escaping () -> Void) {
+        DispatchQueue.main.asyncAfter(deadline: .now() + seconds) {
+            closure()
+        }
     }
 }
 
