e91c0ec43cb8aadd545cf240e19c2071075d1883
Serhii Lukashchuk <s.lukaschuk@yahoo.com>
2025-01-04 23:46:52 -0500

    delay result with async throws

--- MasterCompletionHandlers/ContentView.swift
+++ MasterCompletionHandlers/ContentView.swift
@@ -22,66 +22,26 @@ struct ContentView: View {
     }
     
     func buttonTapped() {
-//        name = instance()
-        
-//        delayed { name in
-//            self.name = name
-//        }
-
-//        delayedOptional { name in
-//            guard let name else {
-//                print("No name")
-//                return
-//            }
-//            self.name = name
-//        }
-        
-//        delayedWithError { result in
-//            if result.1 != nil {
-//                print("Error: \(result.1!.localizedDescription)")
-//                return
-//            } else {
-//                name = result.0
-//            }
-//        }
-        
-        delayedResult { result in
-            switch result {
-            case .success(let name):
-                self.name = name
-            case .failure(let error):
-                print("Error: \(error.localizedDescription)")
+        Task {
+            do {
+                name = try await delayedResult()
+            } catch {
+                print(error.localizedDescription)
             }
         }
     }
     
-//    func instance() -> String {
-//        "Ada"
-//    }
-    
-    
-//    func delayed(completion: @escaping (String) -> Void) {
-//        delay(2) {
-//            completion("Ada")
-//        }
-//    }
-    
-    
-//    func delayedOptional(completion: @escaping (String?) -> Void) {
-//        delay(2) {
-//            completion(nil)
-//        }
-//    }
-    
-//    func delayedWithError(completion: @escaping ((String, Error?)) -> Void) {
-//        delay(2) {
-//            completion(("Ada", nil))
-//        }
-//    }
-    
     func delayedResult(completion: @escaping (Result<String, Error>) -> Void) {
         delay(2) {
             completion(.success("Ada"))
+        }
+    }
+    
+    func delayedResult() async throws -> String {
+        try await withCheckedThrowingContinuation { continuation in
+            delayedResult { result in
+                continuation.resume(with: result)
+            }
         }
     }
     
